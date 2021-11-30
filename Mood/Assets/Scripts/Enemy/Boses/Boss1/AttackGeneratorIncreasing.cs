35f5aac809fe484a1408768df3e9c184b0fc69df
009Schmitt <34982675+009Schmitt@users.noreply.github.com>
2021-11-30 17:06:30 -0300

    Boss animations

---
+++ Mood/Assets/Scripts/Enemy/Boses/Boss1/AttackGeneratorIncreasing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackGeneratorIncreasing : MonoBehaviour
+{
+    private bool isIncreasing; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isIncreasing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
